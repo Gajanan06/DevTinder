8bb351a0c6297238940ed0c0505801c07a3e9d1c
Gajanan06 <gajanannilajkar06@gmail.com>
2026-04-26 17:21:47 +0530

    Explored Dynamic Routing

--- src/app.js
+++ src/app.js
@@ -2,16 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/user', (req, res) => {
+app.get('/user/:userId/:name/:password', (req, res) => {
+    console.log(req.params);
     res.send({firstname: "Gajanan",lastname:"Nilajkar"});
-});
-
-app.post('/user', (req, res) => {
-    res.send('Saved data to the Database');
-});
-
-app.delete('/user', (req, res) => {
-    res.send("Deleted Successfully");
 });
 
 app.listen(3000, () => {
