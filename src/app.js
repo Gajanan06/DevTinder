7d26ca8b3b089b34cea237c4e7e39c62ddc1e847
Gajanan06 <gajanannilajkar06@gmail.com>
2026-04-27 11:28:46 +0530

    Read data from Database using postman

--- src/app.js
+++ src/app.js
@@ -19,6 +19,51 @@ app.post("/signup", async (req, res) => {
     }
 });
 
+app.get("/user", async (req,res) => {
+    const UserEmailId = req.body.emailID;
+    try {
+        const user = await User.findOne({emailID: UserEmailId});
+        if (user.length === 0){
+            res.status(404).send("User not found");
+        } else {
+            res.send(user);
+        }
+    } catch (err) {
+        console.error("Error fetching user:", err);
+        res.status(500).send("Internal Server Error");
+    }   
+});
+
+app.get("/feed", async (req,res) => {
+     
+    try {
+        const user = await User.find({});
+        if (user.length === 0){
+            res.status(404).send("User not found");
+        } else {
+            res.send(user);
+        }
+    } catch (err) {
+        console.error("Error fetching user:", err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.get("/id", async (req,res) => {
+
+    try {
+        const user = await User.findById({_id: req.body._id});
+        if (user.length === 0){
+            res.status(404).send("User not found");
+        } else {
+            res.send(user);
+        }
+    } catch (err) {
+        console.error("Error fetching user:", err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
 mongoDB().then(() => {
     console.log("Connected to MongoDB");
     app.listen(3000, () => {
